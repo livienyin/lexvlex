9b80fcb4301654f40994d7ff6002cfc9aa3f3874
Lyzi Diamond <lyzidiamond@gmail.com>
2014-06-06 09:49:54 -0700

    Add reference object for named statistics

--- js/parks_map.js
+++ js/parks_map.js
@@ -1,3 +1,19 @@
+var statistics = {
+  "activities": "Programming Activities",
+  "picnic": "Picnic Areas",
+  "playgrounds": "Parks with Playgrounds",
+  "dog_parks": "Dog Parks",
+  "tract_name": "Census Tract",
+  "restrooms": "Parks with Restrooms",
+  "trail": "Miles of Trails (in Parks)",
+  "event_space": "Event Spaces (in Parks)",
+  "number_of_parks": "Total Parks",
+  "fields": "Sports Fields/Courts",
+  "total_park_acres": "Total Park Acreage",
+  "biggest_park": "Largest Park by Area",
+  "gardens": "Commnity Gardens"
+}
+
 var map = L.map('map', {
   zoom: 11,
   center: [38.042, -84.515],
